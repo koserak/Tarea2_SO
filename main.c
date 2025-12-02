422d2e34eb0763d9b77ac6e547a22a98603cfcde
SebastianNovaOne <sebaxnovax@gmail.com>
2025-12-01 23:00:27 -0300

    [FEAT] Se añade metodo main para ejecutar Parte I de la tarea

--- main.c
+++ main.c
@@ -1,47 +1,53 @@
 #include <pthread.h>
-#include "barrera.h"
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include "barrera.h"
 
-#define N 5
-#define ETAPAS 4
+#define N 5 // Los threads que se van a sincronizar
+#define ETAPAS 4 // Veces que la barra se reutiliza
 
 typedef struct {
     int id_hilo;
     barrera_t *la_barrera;
 } args_hilo_t;
 
-
 void *trabajador(void *arg){
-	args_hilo_t *datos = (args_hilo_t*) arg;
-	int id = datos->id_hilo;
-	barrera_t *barrera = datos->la_barrera;
-	printf("Soy el hilo %d y voy a esperar...\n", id);
-	for(int i=0, i < N, i++){
+    args_hilo_t *datos = (args_hilo_t*) arg;
+    int id = datos->id_hilo;
+    barrera_t *barrera = datos->la_barrera;
 
-	}
-	return NULL;
+    for(int i = 0; i < ETAPAS; i++){ // cada thread pasa por la barrera ETAPAS veces
+        printf("Hilo %d: Llegué a la barrera en la etapa %d\n", id, i);
+        waitBarrera(barrera); // Esta barrera sincroniza los hilos, todos llegan aqui antes de seguir
+        printf("Hilo %d: Cruzó la barrera en la etapa %d\n", id, i);
+    }
+    return NULL;
 }
 
-int main(int argc, char const *argv[])
-{
-	
+int main() {
 
-	return 0;
+    pthread_t hilos[N]; // Arreglo de los ids de los threads
+    args_hilo_t args[N]; // Arreglo de los argumentos para cada thread
+
+    barrera_t mi_barrera;
+    initBarrera(&mi_barrera, N); // Se inicializa la barrera para N threads
+
+    // Aqui se crean los N hilos
+    for(int i = 0; i < N; i++){
+        args[i].id_hilo = i;
+        args[i].la_barrera = &mi_barrera;
+
+        pthread_create(&hilos[i], NULL, trabajador, &args[i]); // Crea cada thread y le pasa los argumentos
+    }
+
+    // En este for se espera a los hilos
+    for(int i = 0; i < N; i++){
+        pthread_join(hilos[i], NULL);
+    }
+
+    destruyeBarrera(&mi_barrera); // Se destruyen los recursos de barrera
+
+    return 0;
 }
 
-/*
-#include <pthread.h>
 
-typedef struct Barrera_t
-{
-    pthread_mutex_t mutex;
-    pthread_cond_t cond;
-    int count;
-    int N;
-    int etapa;
-} barrera_t;
-
-void initBarrera(barrera_t *B,int numT);
-void waitBarrera(barrera_t *B);
-void destruyeBarrera(barrera_t *B);*/
